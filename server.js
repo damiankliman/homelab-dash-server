01641bdb6c2991ffaae452320df28d1af7a8d489
damiankliman <klimandamian@gmail.com>
2022-07-03 19:28:27 -0700

    fix path to app folders

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, "app")));
+app.use(express.static(path.join(__dirname, "app/homelab-dash", "build")));
 
 app.use("/api/servers", serversRouter);
 app.use("/api/status", statusRouter);
